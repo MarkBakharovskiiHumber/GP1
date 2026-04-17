95c9a1892c0db7aa45a0812d688a1bbf34d53636
MarkBakharovskiiHumber <Bma20.02@mail.ru>
2026-04-17 13:59:41 -0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,17 @@
+"use client";
 import Link from "next/link";
+import { useCart } from "../context/CartContext";
 
 export default function Navbar() {
+  const { cart } = useCart();
+
   return (
     <nav className="navbar">
       <h2>Store</h2>
       <div>
         <Link href="/">Home</Link>
         <Link href="/products">Products</Link>
-        <Link href="/cart">Cart</Link>
+        <Link href="/cart">Cart ({cart.length})</Link>
       </div>
     </nav>
   );
