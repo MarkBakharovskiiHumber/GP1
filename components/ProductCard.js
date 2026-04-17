4cb455f5798a0b45f3d3ffc1c0625230761d72a6
MarkBakharovskiiHumber <Bma20.02@mail.ru>
2026-04-17 13:59:43 -0400

    Update ProductCard.js

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,9 +1,16 @@
-export default function ProductCard({ product, addToCart }) {
+"use client";
+import Link from "next/link";
+import { useCart } from "GP1\context\CartContext.js";
+
+export default function ProductCard({ product }) {
+  const { addToCart } = useCart();
+
   return (
     <div className="card">
       <h3>{product.name}</h3>
       <p>${product.price}</p>
       <button onClick={() => addToCart(product)}>Add to Cart</button>
+      <Link href={`/products/${product.id}`}>View Details</Link>
     </div>
   );
 }
