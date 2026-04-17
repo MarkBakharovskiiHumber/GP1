9808322690342633083134824c193d02b998b10d
MarkBakharovskiiHumber <Bma20.02@mail.ru>
2026-04-17 13:34:35 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,29 @@
+"use client";
+import { useState } from "react";
+import ProductCard from "GP1\components\ProductCard.js";
+
+export default function Home() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+    alert(`${product.name} added to cart`);
+  };
+
+  const products = [
+    { id: 1, name: "Shoes", price: 50 },
+    { id: 2, name: "T-Shirt", price: 25 },
+    { id: 3, name: "Hat", price: 15 },
+  ];
+
+  return (
+    <div className="container">
+      <h1>Welcome to Our Store</h1>
+      <div className="grid">
+        {products.map((p) => (
+          <ProductCard key={p.id} product={p} addToCart={addToCart} />
+        ))}
+      </div>
+    </div>
+  );
+}
