4b644fffff6581c45bb5e7d65ca7e99578fa9270
MarkBakharovskiiHumber <Bma20.02@mail.ru>
2026-04-17 13:59:31 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,18 @@
 import "GP1\styles\globals.css";
 import Navbar from "GP1\components\Navbar.js";
 import Footer from "GP1\components\Footer.js";
+import { CartProvider } from "GP1\context\CartContext.js";
 
-export const metadata = {
-  title: "E-commerce Store",
-  description: "Simple Next.js store",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
+        <CartProvider>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </CartProvider>
       </body>
     </html>
   );
