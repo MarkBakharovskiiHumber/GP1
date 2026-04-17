f476d7de87105faa642a5a3f592ba1f4ebbea6cd
MarkBakharovskiiHumber <Bma20.02@mail.ru>
2026-04-17 13:59:33 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,18 @@
 import { useState } from "react";
 import ProductCard from "GP1\components\ProductCard.js";
 
+import ProductList from "GP1\components\ProductList.js";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Welcome to Our Store</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+
 export default function Home() {
   const [cart, setCart] = useState([]);
 
